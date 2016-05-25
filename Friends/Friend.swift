6c273cd68200ed5efc87e2ee9a459002fdd7babd
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2016-05-25 11:17:31 +1000

    Got image in table view working

---
+++ Friends/Friend.swift
@@ -0,0 +1,34 @@
+//
+//  Friend.swift
+//  Friends
+//
+//  Created by Anis on 24/05/2016.
+//  Copyright © 2016 Anis. All rights reserved.
+//
+
+import Foundation
+
+class Friend {
+    var firstName: String
+    var lastName: String
+    var address: String
+    var flikr: String
+    var website: String
+    var photoUrl: String
+    var photoData: NSData?
+    
+    init(firstName: String, lastName: String, address: String, flikr: String, website: String, photoUrl: String){ //initialising variables
+        self.firstName = firstName
+        self.lastName = lastName
+        self.address = address
+        self.flikr = flikr
+        self.website = website
+        self.photoUrl = photoUrl
+    }
+    
+    func fullName() -> String {
+        return firstName + " " + lastName;
+    }
+    
+    
+}
---
+++ Friends/FriendList.swift
@@ -0,0 +1,13 @@
+//
+//  FriendList.swift
+//  Friends
+//
+//  Created by Anis on 24/05/2016.
+//  Copyright © 2016 Anis. All rights reserved.
+//
+
+import Foundation
+
+class FriendList {
+    var entries = [Friend]() //creates the contact list array
+}
